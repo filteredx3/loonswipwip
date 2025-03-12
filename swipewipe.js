3b508057453763b7926b590917a71db3b8d546df
filteredx3 <65685268+filteredx3@users.noreply.github.com>
2025-03-12 05:45:55 +0100

    Update swipewipe.js

--- swipewipe.js
+++ swipewipe.js
@@ -9,6 +9,19 @@ const targetHost = "itunes.apple.com";
 const scriptName = "iTunes Lookup Mock";
 const script = `
 function mockResponse() {
+  // Generate current date in the correct format (RFC 1123 format)
+  const now = new Date();
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  
+  const formattedDate = days[now.getUTCDay()] + ", " +
+                        String(now.getUTCDate()).padStart(2, '0') + " " +
+                        months[now.getUTCMonth()] + " " +
+                        now.getUTCFullYear() + " " +
+                        String(now.getUTCHours()).padStart(2, '0') + ":" +
+                        String(now.getUTCMinutes()).padStart(2, '0') + ":" +
+                        String(now.getUTCSeconds()).padStart(2, '0') + " GMT";
+  
   const headers = {
     "Server": "daiquiri/5",
     "Content-Type": "text/html",
@@ -16,7 +29,7 @@ function mockResponse() {
     "Location": "https://itunes.apple.com/lookup?bundleId=com.aoklab.fewerphotos&country=FR",
     "X-Apple-Jingle-Correlation-Key": "CVBAJGG6GMMSCOOOMQIEH6DIRM",
     "x-daiquiri-instance": "daiquiri:12282005:mr47p00it-qujn02122302:7987:25RELEASE27:daiquiri-amp-store-l7shared-ext-001-mr",
-    "Date": "Wed, 12 Mar 2025 04:16:36 GMT",
+    "Date": formattedDate,
     "X-Cache": "TCP_REFRESH_MISS from a95-101-8-184.deploy.akamaitechnologies.com (AkamaiGHost/22.0.0-bb2a2774a48042877147cbed2bad8ca6) (S)",
     "X-True-Cache-Key": "/L/itunes.apple.com/lookup vcd=2897 ci2=bundleId=com.aoklab.fewerphotos&country=FR///",
     "Connection": "keep-alive",
@@ -38,16 +51,10 @@ function mockResponse() {
 var $done = mockResponse();
 `;
 
-// URL rewrite rule
-const urlRewriteRule = `^http://itunes\\.apple\\.com/lookup\\?bundleId=com\\.aoklab\\.fewerphotos&country=FR url script-response-header ${scriptName}`;
-
-// Host configuration
-const hostConfig = `hostname = ${targetHost}`;
-
 // Full Loon configuration 
 const fullConfig = `
 #!name=iTunes Lookup Mock
-#!desc=Mock response for iTunes lookup API for Fewer Photos app
+#!desc=Mock response for iTunes lookup API for Fewer Photos app with dynamic date
 
 [Script]
 http-response ${targetURL} script-path=${scriptName}.js, requires-body=false, timeout=10, tag=${scriptName}
